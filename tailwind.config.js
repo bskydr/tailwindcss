4b1e5f79b12500a0fc41fb370f80fb2e6b179ec0
bskydr <62293824+bskydr@users.noreply.github.com>
2026-03-30 16:01:30 -0400

    npm tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./property-details.html",
+    "./sign-in.html",
+    "./Vacation-rentals-website.html"
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '400px',
+        '3xl': '1920px',
+      },
+      colors: {
+        'clifford': '#da373d',
+      },
+    },
+  },
+  plugins: [],
+}
